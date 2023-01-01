944c41ed96abb7646d08146cddc6451caae10e02
lgflorentino <82384021+lgflorentino@users.noreply.github.com>
2023-01-02 09:50:32 +1000

    fix os detection in custom install script

--- custom-extension-scripts/os-specific-commands.sh
+++ custom-extension-scripts/os-specific-commands.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 echo "running $0"
-if [ "$(uname -a | grep Darwin; echo $?)" ]; then 
+if [[ "$(uname -a | grep Darwin)" ]]; then 
     echo "dotbot: Darwin found, running darwin shell commands"
     : 
-elif [ "$(uname -a | grep Linux; echo $?)" ]; then 
+elif [[ "$(uname -a | grep Linux)" ]]; then 
     echo "dotbot: Linux found, running linux shell commands"
     dconf load /com/gexperts/Tilix/ < config/tilix/tilix.dconf
 fi
