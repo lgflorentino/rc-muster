316566c7da05d7fed3224fcc9a1420729d11a061
lgflorentino <82384021+lgflorentino@users.noreply.github.com>
2025-05-10 15:22:34 +1000

    add new configuration passing for Lazy

--- config/neovim/nvim/lua/plugins.lua
+++ config/neovim/nvim/lua/plugins.lua
@@ -27,5 +27,9 @@ return {
             {'nvim-tree/nvim-web-devicons'},
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
         }
+    },
+    {
+        'lgflorentino/cicode.vim',
+        dev = true
     }
 }
